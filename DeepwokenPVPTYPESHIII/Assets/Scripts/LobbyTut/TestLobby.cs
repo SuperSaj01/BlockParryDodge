94a692c76ffc4813771742e5bd4aede17fb5d229
SuperSaj01 <saj.kha01@gmail.com>
2024-12-20 19:02:26 +0000

    Added Relay(2)

--- DeepwokenPVPTYPESHIII/Assets/Scripts/LobbyTut/TestLobby.cs
+++ DeepwokenPVPTYPESHIII/Assets/Scripts/LobbyTut/TestLobby.cs
@@ -4,7 +4,13 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
 
 public class TestLobby : MonoBehaviour
 {
@@ -13,6 +19,7 @@ public class TestLobby : MonoBehaviour
     private Lobby joinedLobby;
     private float heartbeatTimer;
     private float lobbyUpdateTimer;
+    public InputField joinCodeField;
 
     private async void Start()
     {
@@ -32,6 +39,15 @@ public class TestLobby : MonoBehaviour
     {
         HandleLobbyHeartbeat();
         HandleLobbyPollForUpdates();
+
+        if(Input.GetKeyDown("Space"))
+        {
+            JoinRelay(joinCodeField.text);
+        }
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            CreateRelay();
+        }
     }
 
     private async void HandleLobbyHeartbeat()
@@ -159,5 +175,54 @@ public class TestLobby : MonoBehaviour
             Debug.Log(e);
         }
     }
+
+    private async void CreateRelay()
+    {
+        try{
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+            allocation.RelayServer.IpV4,
+            (ushort)allocation.RelayServer.Port,
+            allocation.AllocationIdBytes,
+            allocation.Key,
+            allocation.ConnectionData
+            );
+
+            NetworkManager.Singleton.StartHost();
+
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    async void JoinRelay(string joinCode)
+    {
+        try{
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+            joinAllocation.RelayServer.IpV4,
+            (ushort)joinAllocation.RelayServer.Port,
+            joinAllocation.AllocationIdBytes,
+            joinAllocation.Key,
+            joinAllocation.ConnectionData,
+            joinAllocation.HostConnectionData
+            );
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    
 
 }
---
+++ DeepwokenPVPTYPESHIII/Assets/Scripts/LobbyTut/TestRelay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Services.Core;
+
+public class TestRelay : MonoBehaviour
+{
+    private async void Start()
+    {
+        
+    }
+
+    
+}
